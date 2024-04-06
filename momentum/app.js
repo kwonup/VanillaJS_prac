5b1fa9a8972fe5c7adff7284c173434f6cf7028c
kwonup <kkwon1122@gmail.com>
2024-04-06 19:53:18 +0900

    commit1

---
+++ momentum/app.js
@@ -0,0 +1,19 @@
+const amIFat = null;
+let something;
+console.log(something, amIFat);
+//true는 켜져있음, false는 꺼져있음 , null은 비어있음, undefined는 변수가 메모리에 만들어졌지만 값이 없는것
+
+//#Array
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sta";
+// const sat = "sta";
+// const sun = "sun";
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+console.log(daysOfWeek[3]);
+const nonsense = [1, 2, "hello", false, true, undefined, "nico"];
+console.log(nonsense);
