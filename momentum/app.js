1c27ed938dfba68b7855f7e37b33358bdb1a6ee4
kwonup <kkwon1122@gmail.com>
2024-04-06 20:13:59 +0900

    Object

--- momentum/app.js
+++ momentum/app.js
@@ -1,19 +1,24 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
-//true는 켜져있음, false는 꺼져있음 , null은 비어있음, undefined는 변수가 메모리에 만들어졌지만 값이 없는것
+//Object
+const pName = "nico";
+const pPoints = 121212;
+const pHandsome = false;
+const pFat = "little bit";
 
-//#Array
-// const mon = "mon";
-// const tue = "tue";
-// const wed = "wed";
-// const thu = "thu";
-// const fri = "fri";
-// const sat = "sta";
-// const sat = "sta";
-// const sun = "sun";
+const p = ["nico", 1212, false, "little bit"];
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-console.log(daysOfWeek[3]);
-const nonsense = [1, 2, "hello", false, true, undefined, "nico"];
-console.log(nonsense);
+const player = {
+    name: "nico",
+    points: 10,
+    handsome: false,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+console.log(player["name"]);
+
+player.points = 14;
+player.lastName = "potato";
+delete player.lastName;
+console.log(player);
