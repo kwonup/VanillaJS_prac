9cc8674e34000816a5cd2f925fee00b8ed66d626
kwonup <kkwon1122@gmail.com>
2024-04-06 20:24:24 +0900

    function

--- momentum/app.js
+++ momentum/app.js
@@ -1,24 +1,18 @@
-//Object
-const pName = "nico";
-const pPoints = 121212;
-const pHandsome = false;
-const pFat = "little bit";
+//function
 
-const p = ["nico", 1212, false, "little bit"];
+function sayHello(name, age) {
+    //매개변수(parameter)
+    console.log(`Hello my name is ${name}!! and I'm ${age}`);
+}
+
+sayHello("영우", 26); //인자(argument)전달
+sayHello("소은", 29);
 
 const player = {
     name: "nico",
-    points: 10,
-    handsome: false,
-    fat: true,
+    sayHello: function (otherPersonName) {
+        console.log("Hello " + otherPersonName + " nice to meet you");
+    },
 };
-
-console.log(player);
 console.log(player.name);
-
-console.log(player["name"]);
-
-player.points = 14;
-player.lastName = "potato";
-delete player.lastName;
-console.log(player);
+player.sayHello("lynn");
