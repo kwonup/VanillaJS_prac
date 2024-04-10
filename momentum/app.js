94475fcb36769c20146e6a3f17b2a33bd2fabda6
kwonup <kkwon1122@gmail.com>
2024-04-11 00:22:29 +0900

    cssInJavascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,42 +1,35 @@
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick() {
+//     const currentColor = h1.style.color;
+//     let newColor;
+//     if (currentColor === "blue") {
+//         newColor = "tomato";
+//     } else {
+//         newColor = "blue";
+//     }
+//     h1.style.color = newColor;
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+
+//step1 : find the element
+//step2 : listen the event
+//step3 : react that event
+
 const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1); //console.dir은 전체요소가 출력(json과같은 트리구조)
-
-function handleh1Click() {
-    console.log("h1 was clicked!");
-    h1.style.color = "blue";
+// function handleTitleClick() {
+//     const clickedClass = "clicked";
+//     if (h1.classList.contains(clickedClass)) {
+//         h1.classList.remove(clickedClass);
+//     } else {
+//         h1.classList.add(clickedClass);
+//     }
+//     console.log(h1.className);
+// }
+function handleTitleClick() {
+    h1.classList.toggle("clicked"); //toggle은 clssList에 클래스가 있는지 확인하여
+    console.log(h1.className);
 }
-//Event: 클릭이벤트
-
-function handleMouseEnter() {
-    console.log("mouse is here");
-    h1.innerText = "Mouse is here!";
-}
-function handleMouseLeave() {
-    console.log("mouse is gone");
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-    alert("copier!");
-}
-function handleWindowOffline() {
-    alert("SOS No wifi!");
-}
-function handleWindowOnline() {
-    alert("All Good!");
-}
-h1.addEventListener("click", handleh1Click);
-//h1.onclick = handleh1Click; //이런방식도 가능 but추천x
-h1.addEventListener("mouseenter", handleMouseEnter);
-
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize); //창의 크기가 변경될 때
-window.addEventListener("copy", handleWindowCopy);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
