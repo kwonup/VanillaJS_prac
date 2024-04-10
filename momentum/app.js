1a085d182a9470a8eacfbb6c0d19e65d16bbd910
kwonup <kkwon1122@gmail.com>
2024-04-11 01:39:39 +0900

    gettingUsername

--- momentum/app.js
+++ momentum/app.js
@@ -1,27 +1,15 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; //대문자로 작성하는이유는 관습, 또한 logininput처럼 중요한정보를 담는것이 아니기 때문에 대문자로 작성.
 
-function onLoginSubmit(tomato) {
-    //함수가 하나의 인자(argument)를 받게함 ->JS로 넘겨줌
-    tomato.preventDefault(); //브라우저의 기본동작을 막아줌 -> form의 기본동작인 submit을 막아줌
-    const username = loginInput.value;
-    console.log(tomato); //event에 대한 정보 출력
-    console.log(username);
-}
-//중요!!!
-//모든 EventListener함수의 "첫번째" 인자는 항상 지금 막 벌어진 일들에 대한 정보
-//그리고 JS가 우리에게 그 정보를 무료로 제공함
-//인자 공간만 제공하면 JS가 알아서 방금 일어난 event에대한 정보를 지닌 인자를 채워넣을거임
-
-function handleLinkClick(event) {
-    //alert("clicked!!"); //링크가 클릭되면 이동하는 기본동작을 alert실행으로 막음 ->경고창의 확인버튼을 눌러야 페이지가 넘어감
+function onLoginSubmit(event) {
     event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`; //h1태그에 input에 입력한 내용 전달
+    greeting.classList.remove(HIDDEN_CLASSNAME); //원래 있던 hidden속성 제거
 }
-
-loginForm.addEventListener("submit", onLoginSubmit); //onLoginSubmit뒤에()를 추가하지않는이유는 바로 실행시키지 않기위함임
-//onLoginSubmit(); //()를 더하면 브라우저가 보자마자 바로 function을 실행시킴
-
-link.addEventListener("click", handleLinkClick);
+loginForm.addEventListener("submit", onLoginSubmit);
