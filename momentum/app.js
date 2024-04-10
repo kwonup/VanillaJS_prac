e36b78a1ee6a264374fa6a5f63cfa0530e6f00f7
kwonup <kkwon1122@gmail.com>
2024-04-11 01:08:36 +0900

    The role of argument in eventListener function

--- momentum/app.js
+++ momentum/app.js
@@ -1,35 +1,18 @@
-// const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
 
-// function handleTitleClick() {
-//     const currentColor = h1.style.color;
-//     let newColor;
-//     if (currentColor === "blue") {
-//         newColor = "tomato";
-//     } else {
-//         newColor = "blue";
-//     }
-//     h1.style.color = newColor;
-// }
-
-// h1.addEventListener("click", handleTitleClick);
-
-//step1 : find the element
-//step2 : listen the event
-//step3 : react that event
-
-const h1 = document.querySelector("div.hello:first-child h1");
-
-// function handleTitleClick() {
-//     const clickedClass = "clicked";
-//     if (h1.classList.contains(clickedClass)) {
-//         h1.classList.remove(clickedClass);
-//     } else {
-//         h1.classList.add(clickedClass);
-//     }
-//     console.log(h1.className);
-// }
-function handleTitleClick() {
-    h1.classList.toggle("clicked"); //toggle은 clssList에 클래스가 있는지 확인하여
-    console.log(h1.className);
+function onLoginSubmit(tomato) {
+    //함수가 하나의 인자(argument)를 받게함 ->JS로 넘겨줌
+    tomato.preventDefault(); //브라우저의 기본동작을 막아줌
+    const username = loginInput.value;
+    console.log(tomato); //event에 대한 정보 출력
+    console.log(username);
 }
-h1.addEventListener("click", handleTitleClick);
+//중요!!!
+//모든 EventListener함수의 "첫번째" 인자는 항상 지금 막 벌어진 일들에 대한 정보
+//그리고 JS가 우리에게 그 정보를 무료로 제공함
+//인자 공간만 제공하면 JS가 알아서 방금 일어난 event에대한 정보를 지닌 인자를 채워넣을거임
+
+loginForm.addEventListener("submit", onLoginSubmit); //onLoginSubmit뒤에()를 추가하지않는이유는 바로 실행시키지 않기위함임
+
+//onLoginSubmit(); //()를 더하면 브라우저가 보자마자 바로 function을 실행시킴
