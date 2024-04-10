95a75e03df0e522c659003cf7af258d1b9d62f2b
kwonup <kkwon1122@gmail.com>
2024-04-11 02:31:50 +0900

    Loading username using localStorage

--- momentum/app.js
+++ momentum/app.js
@@ -2,14 +2,32 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"; //대문자로 작성하는이유는 관습, 또한 logininput처럼 중요한정보를 담는것이 아니기 때문에 대문자로 작성.
+const HIDDEN_CLASSNAME = "hidden"; //string을 반복적으로 사용해야할때 다음과 같이 작성, 대문자로 작성하는이유는 관습, 또한 logininput처럼 중요한정보를 담는것이 아니기 때문에 대문자로 작성.
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
+    //제출되었을때 동작 제어
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`; //h1태그에 input에 입력한 내용 전달
-    greeting.classList.remove(HIDDEN_CLASSNAME); //원래 있던 hidden속성 제거
+    localStorage.setItem(USERNAME_KEY, username); //브라우저의 localStorage에 데이터 저장
+    paintGreetings(username);
 }
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+    //h1 hidden속성제거하고 username삽입하는 함수
+    greeting.innerText = `Hello ${username}`; //h1에 저장된 username삽입
+    greeting.classList.remove(HIDDEN_CLASSNAME); //h1의 히든속성 제거
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY); //localStorage에 저장된 username
+if (savedUsername === null) {
+    //로컬에 안 저장되어 있다면
+    loginForm.classList.remove(HIDDEN_CLASSNAME); //loginForm 히든속성 제거
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //로컬에 저장되어 있다면
+    paintGreetings(savedUsername);
+}
